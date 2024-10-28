119abd6c124ef752338308d8d08b374da22372fe
dpvasani <vadanidarshan@gmail.com>
2024-10-28 23:46:08 +0530

    Deploy Setup In Cors And Axios

--- Client/src/hooks/useAxiosPublic.jsx
+++ Client/src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
+    // baseURL: 'http://localhost:5000'
+    baseURL: 'https://darshika-restaurant-backend.onrender.com'
 })
 
 const useAxiosPublic = () => {
--- Client/src/hooks/useAxiosSecure.jsx
+++ Client/src/hooks/useAxiosSecure.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    // baseURL: 'http://localhost:5000'
+    baseURL: 'https://darshika-restaurant-backend.onrender.com'
 })
 
 const useAxiosSecure = () => {
